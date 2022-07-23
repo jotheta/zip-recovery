290b55bb1d0f3bbdf9d262c45b3abd2f477baf5a
Johannes Tafferner <48291408+jotheta@users.noreply.github.com>
2022-07-23 13:13:47 +0200

    Add files via upload

---
+++ zip_recovery.py
@@ -0,0 +1,64 @@
+import zipfile
+import itertools
+import multiprocessing as mp
+import time
+import sys
+import hashlib
+
+# Specify some initial values such as the file name
+
+input_file_name = 'file.zip'
+extensive_input_charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._*+,!?/:'
+input_charset = 'abcdefghijklmnopqrstuvwxyz'
+
+#####################################################################################
+
+def unzip(password):
+
+# Why is the name 'file' required after the 'as'
+	try:
+		
+		wrong_password = False
+		with zipfile.ZipFile(input_file_name) as file:
+	
+			file.extractall(pwd = bytes(password, 'utf-8'))
+						
+	except:
+		
+		wrong_password = True
+		return 'wrong password'
+	
+	finally:
+		
+		# Will only be executed if their is no error thrown
+		if not wrong_password:	
+			#print('correct password')
+			return 'correct password'
+
+	
+#####################################################################################
+
+# Need to add the option of multithreading as well as running the code on the GPU
+	
+def crack_zip(start = 1, end = 2, file_name = input_file_name, charset = input_charset):
+	
+	total_iterations = sum([len(charset)**x for x in range(end + 1)])
+	count = 1
+	
+	for iteration in range(start, end + 1):
+		
+		temp_permutations = itertools.product(charset, repeat = iteration)
+		
+		for p in temp_permutations:
+			
+			current_password = ''.join(p)
+			
+			result = unzip(current_password)
+			print(current_password + ' - ' + result + ' - ' + str(count) + ' of ' + str(total_iterations) + ' - {}%'.format(round(count / total_iterations, 2)))
+			
+			count += 1
+			if result == 'correct password':
+				return current_password
+
+#crack_zip(1,3)
+
